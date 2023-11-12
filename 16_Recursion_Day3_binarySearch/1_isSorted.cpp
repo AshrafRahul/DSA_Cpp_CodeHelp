74fa4896ab198f18619c8e9a16b15b2a19ac11f5
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2023-11-12 23:03:12 +0600

    recursion day 3

---
+++ 16_Recursion_Day3_binarySearch/1_isSorted.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int arr[], int size){ //we can write here *arr
+    //base case
+    if(size==0 || size==1) return true;
+
+    if(arr[0] > arr[1]) return false;
+    else{
+        bool remainPart = isSorted(arr+1, size-1);
+        return remainPart;
+    }
+}
+
+int main(){
+    int arr[5] = {2,5,6,8,9};
+    int size = 5;
+
+    bool ans = isSorted(arr, size);
+
+    if(ans) {
+        cout << "Array is sorted" << endl;
+    } else {
+        cout << "Array is not sorted" << endl;
+    }
+
+    return 0;
+}
